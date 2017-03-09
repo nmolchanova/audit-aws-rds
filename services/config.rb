fdf9dbe650e0ba3eb15556a33f2761fedb5b0aaf
Michael Pautov <michael.pautov@gmail.com>
2017-03-09 13:51:06 +0600

    Change * to latest

--- services/config.rb
+++ services/config.rb
@@ -98,7 +98,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-rds" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "*"
+                   :version => "latest"
                },
                {
                    :name => "js-yaml",
