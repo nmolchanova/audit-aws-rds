f17c19838e456f4f3f69f0f8e00e50a8010b9320
omurbek.jk@gmail.com <omurbek.jk@gmail.com>
2017-07-26 12:04:20 +0600

    CON-604-update jsrunner to 1.10.beta59

--- services/config.rb
+++ services/config.rb
@@ -102,7 +102,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-rds" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.10.7-beta57"
+                   :version => "1.10.7-beta59"
                },
                {
                    :name => "js-yaml",
